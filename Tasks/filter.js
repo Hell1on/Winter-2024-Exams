82c80dd7e62fab5bd19c40a6c1c7bce430a2642c
Artem <107429202+Hell1on@users.noreply.github.com>
2024-01-13 20:43:14 +0000

    rewrote code using filter

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -2,15 +2,7 @@
 'use strict'
 
 const filterArray = (incomingArr, type) => {
-  const modifiedArr = [];
-  
-  for (const element of incomingArr) {
-    const typeOfElement = typeof element;
-    
-    if (typeOfElement === type) modifiedArr.push(element);
-  }
-  
-  return modifiedArr;
+  return incomingArr.filter((element) => typeof element === type)
 };
 
 module.exports = filterArray;
