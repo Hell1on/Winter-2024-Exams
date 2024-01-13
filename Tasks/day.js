93f88169d0ba08820da5e853f4d3befe4b0509ac
Artem <107429202+Hell1on@users.noreply.github.com>
2024-01-13 13:19:02 +0000

    moved days into function and added empty lines

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,13 +1,15 @@
 // Get day number
 'use strict';
 
-const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
 const parseDay = (string) => {
+  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  
   for (let i = 0; i < days.length; i++) {
     const lowerCaseDay = days[i].toLowerCase();
+    
     if (!string.indexOf(lowerCaseDay)) return i + 1;
   }
+  
   return -1;
 };
 
