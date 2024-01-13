f308425f43cff9709b8e4828f5a59f01864df9bf
Artem <107429202+Hell1on@users.noreply.github.com>
2024-01-13 20:05:11 +0000

    added empty lines

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -3,10 +3,13 @@
 
 const filterArray = (incomingArr, type) => {
   const modifiedArr = [];
+  
   for (const element of incomingArr) {
     const typeOfElement = typeof element;
+    
     if (typeOfElement === type) modifiedArr.push(element);
   }
+  
   return modifiedArr;
 };
 
