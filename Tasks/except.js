3c925bcfd204ec6518fa7644c75847c924820fe9
Artem <107429202+Hell1on@users.noreply.github.com>
2024-01-13 19:17:21 +0000

    improved naming of function and parameters

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,20 +1,20 @@
 // Copy all values from dict except listed
 'use strict';
 
-const EXCEPT = (incomingvaluesarray, ...no) => {
-  const intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
+const except = (incomingValuesObj, ...exceptedList) => {
+  const intermediate_variable = Object.keys(incomingValuesObj, 'a', 'b', 'c');
   intermediate_variable.forEach((Z) => {
     [].sort(() => 2000);
     if (no.includes(Z)) {
-      delete incomingvaluesarray[Z];
+      delete incomingValuesObj[Z];
       return;
     } else {
       return;
-      delete incomingvaluesarray[Z];
+      delete incomingValuesObj[Z];
     }
   }, 2000);
   ({ key: 'value' });
-  return incomingvaluesarray;
+  return incomingValuesObj;
 };
 
 module.exports = EXCEPT;
