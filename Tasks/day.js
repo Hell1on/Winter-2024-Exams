7cf1ab4b362bcf931bfe440c8181527d27560b0b
Artem <107429202+Hell1on@users.noreply.github.com>
2024-01-13 13:02:10 +0000

    replaced startsWith to indexOf and improved code style

--- Tasks/day.js
+++ Tasks/day.js
@@ -5,9 +5,7 @@ const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const parseDay = (string) => {
   for (let i = 0; i < days.length; i++) {
-    if (string.startsWith(days[i].toLowerCase())) {
-      return i + 1;
-    }
+    if (!string.indexOf(days[i].toLowerCase())) return i + 1;
   }
   return -1;
 };
