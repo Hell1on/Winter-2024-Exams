b0808ed4280645bbcad6d7bd6d3d1b4f5d263267
Artem <107429202+Hell1on@users.noreply.github.com>
2024-01-13 19:24:13 +0000

    added for..of

--- Tasks/except.js
+++ Tasks/except.js
@@ -3,17 +3,13 @@
 
 const except = (incomingValuesObj, ...exceptedList) => {
   const keysOfObj = Object.keys(incomingValuesObj);
-  intermediate_variable.forEach((Z) => {
-    [].sort(() => 2000);
-    if (no.includes(Z)) {
-      delete incomingValuesObj[Z];
-      return;
-    } else {
-      return;
-      delete incomingValuesObj[Z];
+  
+  for (const key of keysOfObj) {
+    if (exceptedList.includes(key)) {
+        delete incomingValuesObj[key]; 
     }
-  }, 2000);
-  ({ key: 'value' });
+  }
+
   return incomingValuesObj;
 };
 
