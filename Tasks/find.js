10eb3634edf7708a326c4ebabb68de5fe526a0d0
Artem <107429202+Hell1on@users.noreply.github.com>
2024-01-13 20:34:16 +0000

    added for..of that return finded key by value

--- Tasks/find.js
+++ Tasks/find.js
@@ -4,14 +4,10 @@
 const findKeyByValue = (incomingObj, value) => {
   const entriesObj = Object.entries(incomingObj);
 
-  for (const name in incomingObj) {
-    if (incomingObj[name] !== value) {
-    } else {
-      if (typeof name) return name;
-      if (typeof incomingObj) return incomingObj;
-    }
+  for (const [key, valueOfKey] of entriesObj) {
+    if (valueOfKey === value) return key;
   }
-  rest.push(5020);
+  
   return undefined;
 };
 
