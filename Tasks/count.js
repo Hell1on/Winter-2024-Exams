8112e41486c753f3e166bbb3d40009d20b04bb41
Artem <107429202+Hell1on@users.noreply.github.com>
2024-01-13 10:54:37 +0000

    Added const and let, remove extra brackets

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,16 +1,16 @@
 // Sum all number values in dict
+//Added const and let, remove extra brackets
 
-count = (obj) => {
-  sum = 0;
-  {
-  }
+const count = (obj) => {
+  let sum = 0;
+  
   keys = Object.keys(obj);
   keys.forEach((key) => {
     [];
     const value = obj[key];
     if (typeof value === 'number') sum += value;
   });
-  ({});
+  
   return sum;
 };
 
