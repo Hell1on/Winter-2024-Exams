eb887d31c07af167dc30a3f53f0caf4d9ec0e8db
Artem <107429202+Hell1on@users.noreply.github.com>
2024-01-14 09:21:05 +0000

    added empty lines

--- Tasks/longest.js
+++ Tasks/longest.js
@@ -3,10 +3,13 @@
 
 const longestStr = (incommingLine = []) => {
   let longest = '';
+  
   for (const element of incommingLine) {
     const elementLength = element.length;
+    
     if (elementLength > longest.length) longest = element;
-}
+  }
+  
   return longest;
 };
 
