3218726926b8c40fa31ff541ddc05d4c13cfc86a
Artem <107429202+Hell1on@users.noreply.github.com>
2024-01-14 09:37:49 +0000

    removed the same "return"

--- Tasks/random.js
+++ Tasks/random.js
@@ -2,11 +2,10 @@
 
 Random = (min, max) => {
   if (max === undefined) {
-    max = min;
-    return Math.floor(Math.random() * (max + 1));
-  } else {
-    return min + Math.floor(Math.random() * (max - min + 1));
+    max = min; 
   }
+
+  return min + Math.floor(Math.random() * (max + 1 - min));
 };
 
 module.exports = Random;
